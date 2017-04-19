06c270dd0232f6077e227fa2423b083e6ac2d758
Eugene Leonovich <eugene.leonovich@helloprint.com>
2017-04-19 17:51:04 +0200

    Test RetryStrategyFactory

---
+++ tests/Unit/Handler/RetryStrategy/RetryStrategyFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tarantool\JobQueue\Tests\Unit\Handler\RetryStrategy;
+
+use PHPUnit\Framework\TestCase;
+use Tarantool\JobQueue\Handler\RetryStrategy\ConstantRetryStrategy;
+use Tarantool\JobQueue\Handler\RetryStrategy\ExponentialRetryStrategy;
+use Tarantool\JobQueue\Handler\RetryStrategy\LinearRetryStrategy;
+use Tarantool\JobQueue\Handler\RetryStrategy\RetryStrategyFactory;
+
+class RetryStrategyFactoryTest extends TestCase
+{
+    /**
+     * @dataProvider provideCreateData
+     */
+    public function testCreate(string $expectedStrategyClass, string $strategyName, array $args = [])
+    {
+        $factory = new RetryStrategyFactory();
+
+        $strategy = func_num_args() === 2
+            ? $factory->create($strategyName)
+            : $factory->create($strategyName, $args);
+
+        $this->assertInstanceOf($expectedStrategyClass, $strategy);
+    }
+
+    public function provideCreateData()
+    {
+        return [
+            [ConstantRetryStrategy::class, RetryStrategyFactory::CONSTANT],
+            [ExponentialRetryStrategy::class, RetryStrategyFactory::EXPONENTIAL],
+            [LinearRetryStrategy::class, RetryStrategyFactory::LINEAR],
+        ];
+    }
+}
