154e79fc3a1cd67be414bf382e87e013a847185d
Eugene Leonovich <eugene.leonovich@helloprint.com>
2017-06-29 10:52:24 +0200

    Remove redundant PHPDoc

--- src/Handler/RetryStrategy/RetryStrategy.php
+++ src/Handler/RetryStrategy/RetryStrategy.php
@@ -4,9 +4,5 @@ namespace Tarantool\JobQueue\Handler\RetryStrategy;
 
 interface RetryStrategy
 {
-    /**
-     * @param int $attempt
-     * @return int|null
-     */
     public function getDelay(int $attempt): ?int;
 }
